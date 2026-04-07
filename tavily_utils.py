a15349c7e001a3d94974db347db9e0c9bb98a9fb
MarioGP02 <99794121+MarioGP02@users.noreply.github.com>
2026-04-07 19:10:26 +0200

    Correccion de la llamada a la API_KEY

--- tavily_utils.py
+++ tavily_utils.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from tavily import TavilyClient
 
-tavily = TavilyClient(api_key=st.secrets["TAVILY_API_KEY"])
+tavily = TavilyClient(api_key=st.secrets["TU_TAVILY_API_KEY"])
 
 def buscar_en_internet(query):
     # Realiza la búsqueda y devuelve un resumen del contenido
